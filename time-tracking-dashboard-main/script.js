f6f408f1ff7ff82f22c0a13eb41adbdb866096a1
damlakara5 <78643920+damlakara5@users.noreply.github.com>
2021-10-05 18:03:43 +0300

    Add files via upload

---
+++ time-tracking-dashboard-main/script.js
@@ -0,0 +1,15 @@
+const daily = document.querySelector(".daily");
+const weekly = document.querySelector(".weekly");
+const Monthly = document.querySelector(".Monthly");
+
+let workDates = [
+  "2021-11-18T21:31:17.178Z",
+  "2021-12-23T07:42:02.383Z",
+  "2021-01-28T09:15:04.904Z",
+  "2021-04-01T10:17:24.185Z",
+];
+
+daily.addEventListener("click", function () {
+  let date = new Date();
+  console.log(date);
+});
